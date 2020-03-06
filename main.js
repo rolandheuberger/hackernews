3750576b9bc855d2b085a07a91113d3834d610c5
Roland Heuberger <roland.heuberger@pixeltex.com>
2020-03-06 21:27:13 +0100

    rename elements for better styling, build styles for list and footer

--- main.js
+++ main.js
@@ -22,9 +22,9 @@ fetch('https://hacker-news.firebaseio.com/v0/newstories.json')
     .catch(function(error) {
         var p = document.createElement('p');
         p.appendChild(
-        document.createTextNode('Error: ' + error.message)
+            document.createTextNode('Error: ' + error.message)
         );
-        document.body.insertBefore(p, myList);
+        document.body.insertBefore(p, newsList);
     });
 
     function getItem( itemID ) {
@@ -46,13 +46,12 @@ fetch('https://hacker-news.firebaseio.com/v0/newstories.json')
                     var score = (json.score == 1) ? ' point' : ' points';
                     var comments = (json.descendants == 1) ? ' comment' : ' comments';
 
-                    listItem.innerHTML = '<h2 class="news-item__title"><a href="' + item.url + '" target="_blank">' + item.title + '</a></h2>';
-                    listItem.innerHTML += showDomain(json.url);
-                    listItem.innerHTML +='<span class="news-item__points"> ' + json.score + score + '</span> ';
-                    listItem.innerHTML +='<span class="news-item__user">by <a href="https://news.ycombinator.com/user?id=' + json.by + '" target="_blank">' + json.by + '</a></span> ';
-                    listItem.innerHTML +='<span class="news-item__time"><a href="https://news.ycombinator.com/item?id=' + json.id + '" target="_blank">' + moment(json.time * 1000).fromNow() + '</a></span> ';
-                    listItem.innerHTML +='<span class="news-item__hide"><a href="https://news.ycombinator.com/hide?id=' + json.id + '&goto=news">hide</a></span> ';
-                    listItem.innerHTML +='<span class="news-item__comments">' + json.descendants + comments + '</span>';
+                    listItem.innerHTML = '<h2 class="ae ae--title"><a href="' + item.url + '" target="_blank">' + item.title + '</a>' + showDomain(json.url) + '</h2>';
+                    listItem.innerHTML +='<span class="ae ae--points">' + json.score + score + '</span>';
+                    listItem.innerHTML +='<span class="ae ae--user">by <a href="https://news.ycombinator.com/user?id=' + json.by + '" target="_blank">' + json.by + '</a></span>';
+                    listItem.innerHTML +='<span class="ae ae--time"><a href="https://news.ycombinator.com/item?id=' + json.id + '" target="_blank">' + moment(json.time * 1000).fromNow() + '</a></span>';
+                    listItem.innerHTML +='<span class="ae ae--hide"><a href="https://news.ycombinator.com/hide?id=' + json.id + '&goto=news">hide</a></span>';
+                    listItem.innerHTML +='<span class="ae ae--comments">' + json.descendants + comments + '</span>';
                     newsList.appendChild(listItem);
                 }
             })
@@ -65,7 +64,7 @@ fetch('https://hacker-news.firebaseio.com/v0/newstories.json')
         if (typeof uri !== 'undefined' && uri.length > 0) {
             domain = uri.match(r)[1];
             
-            return '<span class="news-item__domain"> (<a href="https://news.ycombinator.com/from?site=' + domain + '">' + domain + '</a>)</span>';
+            return '<span class="ae ae--domain">(<a href="https://news.ycombinator.com/from?site=' + domain + '">' + domain + '</a>)</span>';
         } 
 
         return '';
