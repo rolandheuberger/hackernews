7910a45e76879383949ca12800afd99cfeb9ec58
Roland Heuberger <roland.heuberger@pixeltex.com>
2020-03-06 18:25:33 +0100

    fix wording for points and comments, add showDomain function that extracts the article domain

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ fetch('https://hacker-news.firebaseio.com/v0/newstories.json')
         // console.log(json);
         var pageids = json.slice(offset, limit)
         pageids.forEach( function( itemID ) {
-            getContent( itemID );
+            getItem( itemID );
         });
     })
     .catch(function(error) {
@@ -27,8 +27,9 @@ fetch('https://hacker-news.firebaseio.com/v0/newstories.json')
         document.body.insertBefore(p, myList);
     });
 
-    function getContent( itemID ) {
+    function getItem( itemID ) {
         var itemUrl = 'https://hacker-news.firebaseio.com/v0/item/';
+        
         fetch(itemUrl + itemID + '.json')
             .then(function(response) {
                 if (!response.ok) {
@@ -39,24 +40,33 @@ fetch('https://hacker-news.firebaseio.com/v0/newstories.json')
             })
             .then(function(json) {
                 var item = json;
-                console.log( item );
-                var listItem = document.createElement('article');
-                listItem.innerHTML = '<h2 class="news-item__title"><a href="' + item.url + '" target="_blank">' + item.title + '</a></h2>';
-                listItem.innerHTML +='<span class="news-item__user">by ' + json.by + '</span>';
-                listItem.innerHTML +='<span class="news-item__time">' + moment(json.time*1000).fromNow() + '</span>';
-                listItem.innerHTML +='<span class="news-item__hide">hide</span>';
-                listItem.innerHTML +='<span class="news-item__comments">' + json.descendants + ' comments</span>';
-                newsList.appendChild(listItem);
+                if (typeof item !== null) {
+                    console.log( item );
+                    var listItem = document.createElement('article');
+                    var score = (json.score == 1) ? ' point' : ' points';
+                    var comments = (json.descendants == 1) ? ' comment' : ' comments';
+
+                    listItem.innerHTML = '<h2 class="news-item__title"><a href="' + item.url + '" target="_blank">' + item.title + '</a></h2>';
+                    listItem.innerHTML += showDomain(json.url);
+                    listItem.innerHTML +='<span class="news-item__points"> ' + json.score + score + '</span> ';
+                    listItem.innerHTML +='<span class="news-item__user">by <a href="https://news.ycombinator.com/user?id=' + json.by + '" target="_blank">' + json.by + '</a></span> ';
+                    listItem.innerHTML +='<span class="news-item__time"><a href="https://news.ycombinator.com/item?id=' + json.id + '" target="_blank">' + moment(json.time * 1000).fromNow() + '</a></span> ';
+                    listItem.innerHTML +='<span class="news-item__hide"><a href="https://news.ycombinator.com/hide?id=' + json.id + '&goto=news">hide</a></span> ';
+                    listItem.innerHTML +='<span class="news-item__comments">' + json.descendants + comments + '</span>';
+                    newsList.appendChild(listItem);
+                }
             })
     }
 
-    // article structure
-    // <article class="news-item">
-    //     <h2 class="news-item__title"></h2>
-    //     <span class="news-item__source"></span>
-    //     <span clas="news-item__points">points</span>
-    //     <span class="news-item__user">by user</span>
-    //     <span class="news-item__time">time</span>
-    //     <span class="news-item__hide">hide</span>
-    //     <span class="news-item__comments">comments</span>
-    // </article>
+    function showDomain(uri){
+        var r = /:\/\/(.[^/]+)/;
+        var domain = '';
+
+        if (typeof uri !== 'undefined' && uri.length > 0) {
+            domain = uri.match(r)[1];
+            
+            return '<span class="news-item__domain"> (<a href="https://news.ycombinator.com/from?site=' + domain + '">' + domain + '</a>)</span>';
+        } 
+
+        return '';
+    }
